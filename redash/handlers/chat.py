9076ac4bf5085d5975d0e294213776a26a528211
ldebele <lemidebele@gmail.com>
2024-05-14 17:51:22 +0300

    added langchain and the connection with postgres to get the data

--- redash/handlers/chat.py
+++ redash/handlers/chat.py
@@ -1,29 +1,63 @@
 from flask import request, jsonify
+from scripts.postgres_utils import connect_to_database
 from redash.handlers.base import (
     BaseResource
 )
 import os
-from openai import OpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
 VARIABLE_KEY = os.environ.get("OPENAI_API_KEY")
-client = OpenAI(
-  api_key=VARIABLE_KEY
-)
+chat = ChatOpenAI(model="gpt-3.5-turbo-1106",api_key=VARIABLE_KEY, temperature=0.2)
+
+
+dbname = os.getenv("DATABASE")
+username = os.getenv("USER_NAME")
+password = os.getenv("PASSWORD")
+host = os.getenv("HOST")
+port = os.getenv("PORT")
+conn =  connect_to_database(host, dbname, username, password, port)
+
 
 class ChatResource(BaseResource):
     def post(self):
         try:
+
+            cur = conn.cursor()
+
+            cur.execute(f"SELECT table_name FROM information_schema.tables WHERE table_schema = '{'my_schema'}'")
+            table_names = [row[0] for row in cur.fetchall()]
+
+            schema_info={}
+            for table_name in table_names:
+                # Get column names for each table
+                cur.execute(f"SELECT column_name FROM information_schema.columns WHERE table_schema = 'my_schema' AND table_name = '{table_name}'")
+                column_info = {row[0]: row[1] for row in cur.fetchall()}
+                
+                schema_info[table_name] = column_info
+            cur.close()
+            conn.close()
+
+
             value = request.get_json()
             question = value.get('question')
-            completion = client.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": "You are a redash visualization assistant, skilled in SQL queries and data visualization. You are only required to give answers for query and data visualization questions. If asked about a topic outside these two, make sure to respond that you have no information regarding that question. I am only here to help you with your query and data visualization questions. When asked to write queries, only provide the code without descriptions."},
-                    {"role": "user", "content": question}
+            messages = [
+                    SystemMessage(content= f'You are a helpful assistant.You will return a sql return a sql query to find the question. The database has the following schemas: {all_csv_info}'),
+                    HumanMessage(content=question),
                 ]
-            )
-            answer = completion.choices[0].message.content
-            response_data = {"answer": answer}
-            return jsonify(response_data), 200
+            
+
+
+            content = chat.invoke(messages).content
+
+            try:
+                start_index = content.index("SELECT")
+                end_index = content.index(";") + 1
+            except ValueError:
+                sql_query = content
+            else:
+                sql_query = content[start_index:end_index]
+            return jsonify(sql_query), 200
+        
         except Exception as error:
             print(error)
             return jsonify({"error": "An error occurred"}), 500
